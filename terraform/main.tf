a427d9a2514c4b0601ce5c0814ef4b8d4744b85b
Matt Williamson <mahwill29@gmail.com>
2024-02-22 06:49:22 -0600

    Cleaning up variable assignments.

--- terraform/main.tf
+++ terraform/main.tf
@@ -37,12 +37,11 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
 
 module "kubernetes" {
     source = "./kubernetes_module"
-    host = azurerm_kubernetes_cluster.aks_cluster.kube_admin_config.0.host
-    username = azurerm_kubernetes_cluster.aks_cluster.kube_admin_config.0.username
-    password = azurerm_kubernetes_cluster.aks_cluster.kube_admin_config.0.password
-    client_certificate = azurerm_kubernetes_cluster.aks_cluster.kube_admin_config.0.client_certificate
-    client_key = azurerm_kubernetes_cluster.aks_cluster.kube_admin_config.0.client_key
-    cluster_ca_certificate = azurerm_kubernetes_cluster.aks_cluster.kube_admin_config.0.cluster_ca_certificate
+    host                   = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
+    username               = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.username
+    password               = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.password
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate)
 }
-
 
