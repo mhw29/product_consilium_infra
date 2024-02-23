8ab92384e2b6971cbfc7f8e9d36c072ae8fbdbf2
Matt Williamson <mahwill29@gmail.com>
2024-02-22 20:51:18 -0600

    removing kubectl execution.

--- terraform/kubernetes_module/main.tf
+++ terraform/kubernetes_module/main.tf
@@ -32,17 +32,4 @@ resource "helm_release" "argocd-staging" {
   values     = [templatefile("./kubernetes_module/argocd/values.yaml", {})]
 }
 
-resource "null_resource" "password" {
-  provisioner "local-exec" {
-    working_dir = "./kubernetes_module/argocd"
-    command     = "kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath={.data.password} | base64 -d > argocd-login.txt"
-  }
-}
-
-resource "null_resource" "del-argo-pass" {
-  depends_on = [null_resource.password]
-  provisioner "local-exec" {
-    command = "kubectl -n argocd delete secret argocd-initial-admin-secret"
-  }
-}
 
