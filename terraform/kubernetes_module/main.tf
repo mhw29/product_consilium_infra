b1526b0d9a43b8b46c8b653156b87cc8bf900fef
Matt Williamson <mahwill29@gmail.com>
2024-02-22 20:47:20 -0600

    Fixing pathing and namespace.

--- terraform/kubernetes_module/main.tf
+++ terraform/kubernetes_module/main.tf
@@ -34,15 +34,15 @@ resource "helm_release" "argocd-staging" {
 
 resource "null_resource" "password" {
   provisioner "local-exec" {
-    working_dir = "./argocd"
-    command     = "kubectl -n argocd-staging get secret argocd-initial-admin-secret -o jsonpath={.data.password} | base64 -d > argocd-login.txt"
+    working_dir = "./kubernetes_module/argocd"
+    command     = "kubectl -n argocd get secret argocd-initial-admin-secret -o jsonpath={.data.password} | base64 -d > argocd-login.txt"
   }
 }
 
 resource "null_resource" "del-argo-pass" {
   depends_on = [null_resource.password]
   provisioner "local-exec" {
-    command = "kubectl -n argocd-staging delete secret argocd-initial-admin-secret"
+    command = "kubectl -n argocd delete secret argocd-initial-admin-secret"
   }
 }
 
