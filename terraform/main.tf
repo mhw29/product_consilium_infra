823c90835f93ed6ab405e7cff123f7f2d6311dd3
Matt Williamson <mahwill29@gmail.com>
2024-02-23 21:08:57 -0600

    Moving to azure free tier.

--- terraform/main.tf
+++ terraform/main.tf
@@ -40,7 +40,7 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
     default_node_pool {
         name                = "default"
         node_count          = 1
-        vm_size             = "Standard_B2s"
+        vm_size             = "Standard_Bpsv2"
         enable_auto_scaling = false
     }
 
