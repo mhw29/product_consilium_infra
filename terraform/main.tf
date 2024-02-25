dfdeb918c853b80a513f79e8ca3679682ca6162b
Matt Williamson <mahwill29@gmail.com>
2024-02-25 12:02:12 -0600

    Associating acr

--- terraform/main.tf
+++ terraform/main.tf
@@ -28,54 +28,39 @@ resource "azurerm_container_registry" "acr" {
     resource_group_name      = azurerm_resource_group.aks_rg.name
     location                 = azurerm_resource_group.aks_rg.location
     sku                      = "Standard"
-    admin_enabled            = false
-}
+    resource "azurerm_kubernetes_cluster" "aks_cluster" {
+        name                = "productconsilium-aks"
+        location            = azurerm_resource_group.aks_rg.location
+        resource_group_name = azurerm_resource_group.aks_rg.name
+        dns_prefix          = "productconsilium"
 
-resource "azurerm_kubernetes_cluster" "aks_cluster" {
-    name                = "productconsilium-aks"
-    location            = azurerm_resource_group.aks_rg.location
-    resource_group_name = azurerm_resource_group.aks_rg.name
-    dns_prefix          = "productconsilium"
+        default_node_pool {
+            name                = "default"
+            node_count          = 1
+            vm_size             = "Standard_B2s"
+            enable_auto_scaling = false
+        }
 
-    default_node_pool {
-        name                = "default"
-        node_count          = 1
-        vm_size             = "Standard_B2s"
-        enable_auto_scaling = false
+        identity {
+            type = "SystemAssigned"
+        }
+
+        addon_profile {
+            acr_integration {
+                enabled = true
+                registry_id = azurerm_container_registry.acr.id
+            }
+        }
     }
 
-    identity {
-        type = "SystemAssigned"
+    module "kubernetes" {
+        source = "./kubernetes_module"
+        host                   = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
+        username               = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.username
+        password               = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.password
+        client_certificate     = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate)
+        client_key             = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key)
+        cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate)
     }
-}
-
-resource "azuread_application" "app" {
-    display_name = "github-action-acr-app"
-}
-
-resource "azuread_service_principal" "sp" {
-    application_id  = azuread_application.app.application_id
-}
-
-resource "azuread_service_principal_password" "sp_password" {
-  service_principal_id         = azuread_service_principal.sp.id
-  end_date_relative            = "8760h" # 1 year
-}
-
-resource "azurerm_role_assignment" "acr_push" {
-  scope                            = azurerm_container_registry.acr.id
-  role_definition_name             = "AcrPush"
-  principal_id                     = azuread_service_principal.sp.id
-}
-
-module "kubernetes" {
-    source = "./kubernetes_module"
-    host                   = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
-    username               = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.username
-    password               = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.password
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate)
-}
 
 
