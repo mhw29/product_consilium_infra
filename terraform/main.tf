e3f6d4a2853eea9bab356ecbbf7c5f856404518f
Matt Williamson <mahwill29@gmail.com>
2024-02-25 18:04:47 -0600

    .

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,12 +50,6 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
 
 }
 
-resource "azurerm_role_assignment" "acr_pull" {
-  scope                = azurerm_container_registry.acr.id
-  role_definition_name = "AcrPull"
-  principal_id         = azurerm_kubernetes_cluster.aks_cluster.kubelet_identity[0].object_id
-}
-
 module "kubernetes" {
     source = "./kubernetes_module"
     host                   = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
