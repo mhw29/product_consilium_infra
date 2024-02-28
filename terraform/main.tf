f1af249b73e9a8c70fc82c75bd24b92a840c2b30
Matt Williamson <mahwill29@gmail.com>
2024-02-28 06:30:32 -0600

    Fixing yaml for application.

--- terraform/main.tf
+++ terraform/main.tf
@@ -50,6 +50,18 @@ resource "azurerm_kubernetes_cluster" "aks_cluster" {
 
 }
 
+resource "azurerm_postgresql_server" "postgres_db" {
+    name                = "productconsilium-postgres"
+    location            = azurerm_resource_group.aks_rg.location
+    resource_group_name = azurerm_resource_group.aks_rg.name
+    sku_name            = "B_Gen5_1"
+    storage_mb          = 5120
+    version             = "11"
+    administrator_login = var.postgres_username
+    administrator_login_password = var.postgres_password
+}
+
+
 # Will revisit this.
 
 # # Use the ACR's ID to get its Resource ID
