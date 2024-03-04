5ece2d670fa1897673cabb470b6736744dd8c281
Matt Williamson <mahwill29@gmail.com>
2024-03-03 21:06:20 -0600

    Fixing provider.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -14,20 +14,20 @@ provider "azurerm" {
 }
 
 provider "kubernetes" {
-  host                   = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
-  username               = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.username
-  password               = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.password
-  client_certificate     = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate)
-  client_key             = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key)
-  cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate)
+  host                   = module.aks.kube_config.0.host
+  username               = module.aks.kube_config.0.username
+  password               = module.aks.kube_config.0.password
+  client_certificate     = base64decode(module.aks.kube_config.0.client_certificate)
+  client_key             = base64decode(module.aks.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(module.aks.kube_config.0.cluster_ca_certificate)
 }
 
 provider "helm" {
   kubernetes {
-    host                   = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate)
+    host                   = module.aks.kube_config.0.host
+    client_certificate     = base64decode(module.aks.kube_config.0.client_certificate)
+    client_key             = base64decode(module.aks.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(module.aks.kube_config.0.cluster_ca_certificate)
   }
 }
 
