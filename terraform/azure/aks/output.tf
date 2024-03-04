64db0bafb1ea56751a0bbc0ba3aaaf3f8843c12b
Matt Williamson <mahwill29@gmail.com>
2024-03-03 21:37:29 -0600

    .

--- terraform/azure/aks/output.tf
+++ terraform/azure/aks/output.tf
@@ -13,37 +13,37 @@ output "principal_id" {
 }
 
 output "host" {
-    value = module.aks.kube_config.0.host
+    value = azurerm_kubernetes_cluster.current.kube_config.0.host
 
     sensitive = true
 }
 
 output "username" {
-    value = module.aks.kube_config.0.username
+    value = azurerm_kubernetes_cluster.current.kube_config.0.username
 
     sensitive = true
 }
 
 output "password" {
-    value = module.aks.kube_config.0.password
+    value = azurerm_kubernetes_cluster.current.kube_config.0.password
 
     sensitive = true
 }
 
 output "client_certificate" {
-    value = base64decode(module.aks.kube_config.0.client_certificate)
+    value = base64decode(azurerm_kubernetes_cluster.current.kube_config.0.client_certificate)
 
     sensitive = true
 }
 
 output "client_key" {
-    value = base64decode(module.aks.kube_config.0.client_key)
+    value = base64decode(azurerm_kubernetes_cluster.current.kube_config.0.client_key)
 
     sensitive = true
 }
 
 output "cluster_ca_certificate" {
-    value = base64decode(module.aks.kube_config.0.cluster_ca_certificate)
+    value = base64decode(azurerm_kubernetes_cluster.current.kube_config.0.cluster_ca_certificate)
 
     sensitive = true
 }
