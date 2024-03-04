039261866be6a66031fbf66cbe4097a8a5e3fa0d
Matt Williamson <mahwill29@gmail.com>
2024-03-04 07:02:53 -0600

    Trying to remove kubernetes.

--- terraform/kubernetes/variables.tf
+++ terraform/kubernetes/variables.tf
@@ -1,30 +1,30 @@
-variable "host" {
-    type = string
-    description = "The Kubernetes cluster host"
-}
+# variable "host" {
+#     type = string
+#     description = "The Kubernetes cluster host"
+# }
 
-variable "username" {
-    type = string
-    description = "The Kubernetes cluster username"
-}
+# variable "username" {
+#     type = string
+#     description = "The Kubernetes cluster username"
+# }
 
-variable "password" {
-    type = string
-    description = "The Kubernetes cluster password"
-}
+# variable "password" {
+#     type = string
+#     description = "The Kubernetes cluster password"
+# }
 
-variable "client_certificate" {
-    type = string
-    description = "The Kubernetes cluster client certificate"
-}
+# variable "client_certificate" {
+#     type = string
+#     description = "The Kubernetes cluster client certificate"
+# }
 
-variable "client_key" {
-    type = string
-    description = "The Kubernetes cluster client key"
-}
+# variable "client_key" {
+#     type = string
+#     description = "The Kubernetes cluster client key"
+# }
 
-variable "cluster_ca_certificate" {
-    type = string
-    description = "The Kubernetes cluster CA certificate"
-}
+# variable "cluster_ca_certificate" {
+#     type = string
+#     description = "The Kubernetes cluster CA certificate"
+# }
 
