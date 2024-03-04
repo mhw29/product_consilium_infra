c6d91edadf2396b24cb527cc8f882015a2b225db
Matt Williamson <mahwill29@gmail.com>
2024-03-04 06:32:12 -0600

    Updating provider back to main directory.

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -13,21 +13,26 @@ provider "azurerm" {
     use_oidc = true
 }
 
-# provider "kubernetes" {
-#   host                   = module.aks.host
-#   username               = module.aks.username
-#   password               = module.aks.password
-#   client_certificate     = module.aks.client_certificate
-#   client_key             = module.aks.client_key
-#   cluster_ca_certificate = module.aks.cluster_ca_certificate
-# }
+data "azurerm_kubernetes_cluster" "default" {
+  depends_on          = [module.aks] # refresh cluster state before reading
+  name                = "productconsilium-aks"
+  resource_group_name = "product-consilium-resource-group"
+}
 
-# provider "helm" {
-#   kubernetes {
-#     host                   = module.aks.host
-#     client_certificate     = module.aks.client_certificate
-#     client_key             = module.aks.client_key
-#     cluster_ca_certificate = module.aks.cluster_ca_certificate
-#   }
-# }
+provider "helm" {
+  kubernetes {
+    host                   = data.azurerm_kubernetes_cluster.default.kube_config.0.host
+    client_certificate     = base64decode(data.azurerm_kubernetes_cluster.default.kube_config.0.client_certificate)
+    client_key             = base64decode(data.azurerm_kubernetes_cluster.default.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.default.kube_config.0.cluster_ca_certificate)
+  }
+}
+
+provider "kubernetes" {
+  host                   = data.azurerm_kubernetes_cluster.default.kube_config.0.host
+  client_certificate     = base64decode(data.azurerm_kubernetes_cluster.default.kube_config.0.client_certificate)
+  client_key             = base64decode(data.azurerm_kubernetes_cluster.default.kube_config.0.client_key)
+  cluster_ca_certificate = base64decode(data.azurerm_kubernetes_cluster.default.kube_config.0.cluster_ca_certificate)
+}
+
 
