907c931cab588bfa8013d02b3fba307e823be80d
Matt Williamson <mahwill29@gmail.com>
2024-03-03 20:54:07 -0600

    Fixing pathing again.

--- terraform/main.tf
+++ terraform/main.tf
@@ -17,12 +17,12 @@ data "azurerm_client_config" "current" {}
 
 module "kubernetes" {
     source = "./kubernetes"
-    host                   = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.host
-    username               = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.username
-    password               = azurerm_kubernetes_cluster.aks_cluster.kube_config.0.password
-    client_certificate     = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_certificate)
-    client_key             = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.client_key)
-    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.aks_cluster.kube_config.0.cluster_ca_certificate)
+    host                   = azurerm_kubernetes_cluster.current.kube_config.0.host
+    username               = azurerm_kubernetes_cluster.current.kube_config.0.username
+    password               = azurerm_kubernetes_cluster.current.kube_config.0.password
+    client_certificate     = base64decode(azurerm_kubernetes_cluster.current.kube_config.0.client_certificate)
+    client_key             = base64decode(azurerm_kubernetes_cluster.current.kube_config.0.client_key)
+    cluster_ca_certificate = base64decode(azurerm_kubernetes_cluster.current.kube_config.0.cluster_ca_certificate)
 }
 
 module "aks" {
@@ -152,6 +152,6 @@ resource "azurerm_resource_group" "current" {
 resource "azurerm_role_assignment" "key_vault_secrets_user" {
   scope                = azurerm_key_vault.key_vault.id
   role_definition_name = "Key Vault Secrets User"
-  principal_id         = azurerm_kubernetes_cluster.aks_cluster.identity[0].principal_id
+  principal_id         = azurerm_kubernetes_cluster.current.identity[0].principal_id
 }
 
