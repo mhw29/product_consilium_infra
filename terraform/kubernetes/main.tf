77cc84693f5fdadafe3c551142e9e19a414782ae
Matt Williamson <mahwill29@gmail.com>
2024-03-04 06:45:34 -0600

    Moving kubernetes work.

--- terraform/kubernetes/main.tf
+++ terraform/kubernetes/main.tf
@@ -1,27 +1,27 @@
-resource "kubernetes_namespace" "argocd" {
-  metadata {
-    name = "argocd"
-  }
-}
+# resource "kubernetes_namespace" "argocd" {
+#   metadata {
+#     name = "argocd"
+#   }
+# }
 
-resource "helm_release" "argocd" {
-  name       = "argocd"
-  chart      = "argo-cd"
-  repository = "https://argoproj.github.io/argo-helm"
-  version    = "5.27.3"
-  namespace  = "argocd"
-  timeout    = "1200"
-  values     = [templatefile("./kubernetes/argocd/values.yaml", {})]
-}
+# resource "helm_release" "argocd" {
+#   name       = "argocd"
+#   chart      = "argo-cd"
+#   repository = "https://argoproj.github.io/argo-helm"
+#   version    = "5.27.3"
+#   namespace  = "argocd"
+#   timeout    = "1200"
+#   values     = [templatefile("./kubernetes/argocd/values.yaml", {})]
+# }
 
-resource "kubernetes_manifest" "namespace" {
-  depends_on = [helm_release.argocd]
-  manifest = yamldecode(templatefile("./kubernetes/argocd/namespace.yaml", {}))
-}
-resource "kubernetes_manifest" "application" {
-  depends_on = [kubernetes_manifest.namespace]
-  manifest = yamldecode(templatefile("./kubernetes/argocd/application.yaml", {}))
-}
+# resource "kubernetes_manifest" "namespace" {
+#   depends_on = [helm_release.argocd]
+#   manifest = yamldecode(templatefile("./kubernetes/argocd/namespace.yaml", {}))
+# }
+# resource "kubernetes_manifest" "application" {
+#   depends_on = [kubernetes_manifest.namespace]
+#   manifest = yamldecode(templatefile("./kubernetes/argocd/application.yaml", {}))
+# }
 
 
 
