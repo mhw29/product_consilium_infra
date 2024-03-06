53f7342f0b7883f5b8343a067d6b656cb0177a1d
Matt Williamson <mahwill29@gmail.com>
2024-03-05 21:29:24 -0600

    Adding kubernetes service account.

--- terraform/main.tf
+++ terraform/main.tf
@@ -135,20 +135,20 @@ resource "kubernetes_namespace" "eso" {
 
 // the `e2e` pod itself runs with workload identity and
 // does not rely on client credentials.
-# resource "kubernetes_service_account" "e2e" {
-#   metadata {
-#     name      = "external-secrets-e2e"
-#     namespace = "default"
-#     annotations = {
-#       "azure.workload.identity/client-id" = module.e2e_sp.application_id
-#       "azure.workload.identity/tenant-id" = data.azurerm_client_config.current.tenant_id
-#     }
-#     labels = {
-#       "azure.workload.identity/use" = "true"
-#     }
-#   }
-#   depends_on = [module.aks, kubernetes_namespace.eso]
-# }
+resource "kubernetes_service_account" "e2e" {
+  metadata {
+    name      = "external-secrets-e2e"
+    namespace = "default"
+    annotations = {
+      "azure.workload.identity/client-id" = module.e2e_sp.application_id
+      "azure.workload.identity/tenant-id" = data.azurerm_client_config.current.tenant_id
+    }
+    labels = {
+      "azure.workload.identity/use" = "true"
+    }
+  }
+  depends_on = [module.aks, kubernetes_namespace.eso]
+}
 
 # resource "kubernetes_service_account" "current" {
 #   metadata {
